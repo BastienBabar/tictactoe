6fb951c5c6d977a2a28cba424fdaeaa5d1ee79bb
Bastien <bastien.babar@gmail.com>
2015-12-08 18:53:29 +0100

    Started board model specs

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
+require 'matrix'
 
 RSpec.describe Board, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'new board' do
+    let(:matrix) { Matrix.build(3) { '_'} }
+    context do
+      b = Board.new
+      it { expect(b.matrix).to eq matrix }
+    end
+  end
 end
