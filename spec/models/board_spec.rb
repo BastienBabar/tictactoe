998fc15a2dd2c394e7bc6c680e57e5322bc99806
Bastien Barbe <bastien.babar@gmail.com>
2015-12-09 01:00:28 +0100

    double quote

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,46 +1,46 @@
-require 'rails_helper'
-require 'matrix'
+require "rails_helper"
+require "matrix"
 
 RSpec.describe Board, type: :model do
-  let(:matrix) { Matrix.build(3) { '_'} }
+  let(:matrix) { Matrix.build(3) { "_"} }
   let(:x)      { 0 }
   let(:y)      { 0 }
-  let(:matrix_marked) { Matrix.rows([['x','_','_'],['_','_','_'],['_','_','_']]).to_a }
-  let(:matrix_full) { Matrix.rows([['x','o','x'],['o','o','x'],['x','o','o']]).to_a }
-  let(:matrix_winning) { Matrix.rows([['x','o','x'],['x','x','o'],['x','o','x']]).to_a }
+  let(:matrix_marked) { Matrix.rows([["x","_","_"],["_","_","_"],["_","_","_"]]).to_a }
+  let(:matrix_full) { Matrix.rows([["x","o","x"],["o","o","x"],["x","o","o"]]).to_a }
+  let(:matrix_winning) { Matrix.rows([["x","o","x"],["x","x","o"],["x","o","x"]]).to_a }
 
-  describe 'board' do
+  describe "board" do
     b = Board.new
-    context 'new' do
+    context "new" do
       it { expect(b.matrix).to eq matrix }
     end
 
-    context 'mark' do
-      before { b.mark(x, y, 'x') }
+    context "mark" do
+      before { b.mark(x, y, "x") }
       it { expect(b.matrix).to eq Board.new(matrix_marked).matrix }
     end
 
-    context 'not finished' do
+    context "not finished" do
       it {
-        finished = b.finished?('x')
+        finished = b.finished?("x")
         expect(finished).to eq false
       }
     end
 
-    context 'finished' do
-      context 'board full' do
+    context "finished" do
+      context "board full" do
         before { b = Board.new(matrix_full) }
         it {
-          finished = b.finished?('x')
-          expect(finished).to eq 'full'
+          finished = b.finished?("x")
+          expect(finished).to eq "full"
         }
       end
 
-      context 'board won' do
+      context "board won" do
         before { b = Board.new(matrix_winning) }
         it {
-          finished = b.finished?('x')
-          expect(finished).to eq 'x'
+          finished = b.finished?("x")
+          expect(finished).to eq "x"
          }
       end
     end
