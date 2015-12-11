504468485254e6540bfbac74929b9f50ee2d1073
Bastien Barbe <bastien.babar@gmail.com>
2015-12-11 17:50:58 +0100

    documented it all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # get coordinates mapping between front end array and board's Matrix object (initializer)
   def get_coordinates(id)
     [APP_BOARD_COORDINATES[id.to_i]['x'], APP_BOARD_COORDINATES[id.to_i]['y']]
   end
--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,8 +1,11 @@
 class GameController < ApplicationController
+  # home page creating a new game
   def new
     render "new"
   end
 
+  # initialize objects and start game
+  # put in session new board, players and current player id
   def init
     session["board"] = @board = Board.new.matrix
     @players = PlayerList.new(params[:x], params[:o])
@@ -10,12 +13,15 @@ class GameController < ApplicationController
     session["current_player"] = @current_player = @players.current_player
   end
 
+  # play function each other players
+  # reuse session to keep the board up to date
+  # reuse session to switch player each round
   def turn
     @board = Board.new session["board"]
     @players = PlayerList.new session["players"][0]["name"],
                               session["players"][1]["name"],
                               session["current_player"]
-    @coordinates = get_coordinates(params[:id]) #if id != nil or ""
+    @coordinates = get_coordinates(params[:id]) #add - if id != nil or "" or not in list
     @symbol = @players.players[session["current_player"].to_i].symbol
     @name = @players.players[session["current_player"].to_i].name
 
@@ -24,6 +30,7 @@ class GameController < ApplicationController
     session["players"] = @players.players
     session["current_player"] = @players.current_player
 
+    # finished parameter to end the game on frontend
     respond_to do |format|
       format.json do
         render json:
